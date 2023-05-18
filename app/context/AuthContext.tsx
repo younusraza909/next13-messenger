d1c303fd21c8ca3303394a0328153a8207d7cf0d
younusraza909 <younusraza909@gmail.com>
2023-05-18 10:45:33 -0700

    checking session and redirecting user

---
+++ app/context/AuthContext.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import { FC } from "react";
+
+interface AuthContextProps {
+  children: React.ReactNode;
+}
+
+const AuthContext: FC<AuthContextProps> = ({ children }) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default AuthContext;
