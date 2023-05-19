691a55ca1cd1a69cbd17dd6f601db1f6789c05b4
younusraza909 <younusraza909@gmail.com>
2023-05-19 11:56:51 -0700

    Added server function for sessions and added avatar

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
 import DesktopSidebar from "./DesktopSidebar";
 import Mobilefooter from "./Mobilefooter";
 
 async function Sidebar({ children }: { children: React.ReactNode }) {
+  const currentUser = await getCurrentUser();
   return (
     <div className="h-full">
-      <DesktopSidebar />
+      <DesktopSidebar currentUser={currentUser} />
       <Mobilefooter />
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    swcPlugins: [["next-superjson-plugin", {}]],
+  },
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
