b18a5367de6d2d6342bb2331a628ab5c3481753f
younusraza909 <younusraza909@gmail.com>
2023-05-25 12:02:19 +0500

    Fixed prisma deployment error

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -5,10 +5,10 @@ import Mobilefooter from "./Mobilefooter";
 async function Sidebar({ children }: { children: React.ReactNode }) {
   const currentUser = await getCurrentUser();
   return (
-    <div className="h-full">
-      <DesktopSidebar currentUser={currentUser} />
+    <div className='h-full'>
+      <DesktopSidebar currentUser={currentUser!} />
       <Mobilefooter />
-      <main className="lg:pl-20 h-full">{children}</main>
+      <main className='lg:pl-20 h-full'>{children}</main>
     </div>
   );
 }
